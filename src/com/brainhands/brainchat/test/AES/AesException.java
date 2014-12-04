4590d89ddf1610c494e55ef7d936ef379f643a42
Mine_Bit <x-manstor@yandex.ru>
2014-12-04 12:43:48 +0400

    Написал реализацию AES шифрования/расшифровки

---
+++ src/com/brainhands/brainchat/test/AES/AesException.java
@@ -0,0 +1,10 @@
+package com.brainhands.brainchat.test.AES;
+
+/**
+ * Created by Василевский on 04.12.2014.
+ */
+public class AesException extends Exception {
+    public AesException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
