8470a5a0fd9daa1829ad1d7195a85e8de0de99f5
Mine_Bit <x-manstor@yandex.ru>
2015-01-20 17:45:42 +0400

    Класс, для хранения перемеменных с данными принятыми с сервера

---
+++ src/com/brainhands/brainchat/ver_02/client/FromServerStrings.java
@@ -0,0 +1,13 @@
+package com.brainhands.brainchat.ver_02.client;
+
+/**
+ * Created by Василевский on 20.01.2015.
+ * Класс с переменными рабитыми по категориям, которые хранят принятую с сервера информацию
+ * После использовария переменной, желательно установть значение null, дабы избежать ошибок
+ **/
+public class FromServerStrings{
+    //Переменная категории регистрация:
+    String string_from_code_0 = null;
+    //Переменная категории авторизация:
+    String string_from_code_1 = null;
+}
